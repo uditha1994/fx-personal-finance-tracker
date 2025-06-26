a33df81bf61e323e7f8cf5bcdbbb44d8629b3a5b
Uditha Landekumbura <uditha@programmer.net>
2025-06-26 14:49:40 +0530

    complete user model

--- src/config/Database.java
+++ src/config/Database.java
@@ -1,4 +1,33 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+
+    private static Database instance;
+    private Connection connection;
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/finance_tracker";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Uditha@321";
+
+    private Database() {}
+
+    public static Database getInstance() {
+        if(instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if(connection == null) {
+            connection = DriverManager.getConnection
+                    (URL, USER, PASSWORD);
+        }
+        return connection;
+    }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,69 @@
 package models;
 
+import java.sql.Timestamp;
+
 public class User {
+    private int user_id;
+    private String full_name;
+    private String email;
+    private String password;
+    private Timestamp creates_at;
+    private Timestamp last_login;
+
+    public User() {
+    }
+
+    public User(String fullname, String email, String password) {
+        this.full_name = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreates_at() {
+        return creates_at;
+    }
+
+    public void setCreates_at(Timestamp creates_at) {
+        this.creates_at = creates_at;
+    }
+
+    public Timestamp getLast_login() {
+        return last_login;
+    }
+
+    public void setLast_login(Timestamp last_login) {
+        this.last_login = last_login;
+    }
 }
